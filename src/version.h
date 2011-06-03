b5570e57bf00c5e18169c0244ea29350730c8f06
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2011-06-03 10:01:52 +0200

    version bump 1.3.2

--- src/version.h
+++ src/version.h
@@ -19,10 +19,10 @@
 #define WACOMTABLETVERSION_H
 
 // Version of the kcontrol module
-static const char kcm_version[] = "1.3.0-beta2";
+static const char kcm_version[] = "1.3.2";
 
 //version of the kde demon
-static const char kded_version[] = "1.3.0-beta2";
+static const char kded_version[] = "1.3.2";
 
 //version of the plasma applet
 //version info is in applet/plasma-applet-wacomtable.desktop
