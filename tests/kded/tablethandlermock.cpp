ab50083a59f59ebe9409adb681beb0794886e7be
Alexander Maret-Huskinson <alex@maret.de>
2012-09-11 00:24:12 +0200

    Added unit test suite for KDEd module.
    Prepared unit tests for the KDE daemon module by adding an entry for a
    static library to its CMakefile. This library is linked against all unit
    tests for the daemon module as we can not link against the module's
    shared object file directly.
    
    Also added a mock object for the tablet handler and a stub for the
    tablet service unit test. This allows us to test if the new library can
    be linked successfully and if all Qt mocs are being generated by the
    cmake macros.

---
+++ tests/kded/tablethandlermock.cpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2012 Alexander Maret-Huskinson <alex@maret.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "debug.h"
+#include "tablethandlermock.h"
+
+using namespace Wacom;
+
+TabletHandlerMock::TabletHandlerMock() : QObject(NULL)
+{
+    m_profile     = QLatin1String("default");
+    m_flagPenMode = false;
+    m_flagTouch   = false;
+
+    m_profiles.append(QLatin1String("default"));
+    m_profiles.append(QLatin1String("myprofile"));
+}
+
+
+TabletHandlerMock::~TabletHandlerMock() {}
+
+
+/*
+void TabletHandlerMock::emitProfileChanged(const QString& profile)
+{
+    emit profileChanged(profile);
+}
+
+
+
+void TabletHandlerMock::emitTabletAdded(const TabletInformation& info)
+{
+    emit tabletAdded(info);
+}
+
+
+
+void TabletHandlerMock::emitTabletRemoved()
+{
+    emit tabletRemoved();
+}
+*/
+
+
+QString TabletHandlerMock::getProperty(const QString& device, const Property& property) const
+{
+    return getProperty(device, property.key());
+}
+
+
+
+QString TabletHandlerMock::getProperty(const QString& device, const QString& property) const
+{
+    //m_lastDeviceGet   = device;
+    //m_lastPropertyGet = property;
+
+    return m_property;
+}
+
+
+
+QStringList TabletHandlerMock::listProfiles() const
+{
+    return m_profiles;
+}
+
+
+
+void TabletHandlerMock::setProfile(const QString& profile)
+{
+    m_profile = profile;
+    //emitProfileChanged(profile);
+}
+
+
+
+void TabletHandlerMock::setProperty(const QString& device, const QString& property, const QString& value)
+{
+    m_lastDeviceSet   = device;
+    m_lastPropertySet = property;
+    m_property        = value;
+}
+
+
+
+void TabletHandlerMock::setProperty(const QString& device, const Property& property, const QString& value)
+{
+    setProperty(device, property.key(), value);
+}
+
+
+
+void TabletHandlerMock::togglePenMode()
+{
+    m_flagPenMode = !m_flagPenMode;
+}
+
+
+
+void TabletHandlerMock::toggleTouch()
+{
+    m_flagTouch = !m_flagTouch;
+}
---
+++ tests/kded/tablethandlermock.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2012 Alexander Maret-Huskinson <alex@maret.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TABLETHANDLERMOCK_H
+#define TABLETHANDLERMOCK_H
+
+#include "tablethandlerinterface.h"
+#include "tabletinformation.h"
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtCore/QStringList>
+
+namespace Wacom
+{
+class TabletHandlerMock : public QObject
+{
+    Q_OBJECT
+
+public:
+    TabletHandlerMock();
+    virtual ~TabletHandlerMock();
+
+    /*
+    //! Emmits a profileChanged signal with the given parameter.
+    void emitProfileChanged(const QString& profile);
+
+    //! Emmits a tabletAdded signal with the given parameter.
+    void emitTabletAdded(const TabletInformation& info);
+
+    //! Emmits a tabletRemoved signal.
+    void emitTabletRemoved();
+    */
+
+    //! Gets the current mock property value no matter which property or device is requested.
+    QString getProperty(const QString& device, const Property& property) const;
+
+    //! Gets the current mock property value no matter which property or device is requested.
+    QString getProperty(const QString& device, const QString& property) const;
+
+    //! Returns the mock's profile list.
+    QStringList listProfiles() const;
+
+    //! Sets the given profile on the mock and emits a profileChanged signal.
+    void setProfile(const QString& profile);
+
+    //! Sets the given property value on the mock no matter which device or property is set.
+    void setProperty(const QString & device, const QString& property, const QString & value);
+
+    //! Sets the given property value on the mock no matter which device or property is set.
+    void setProperty(const QString& device, const Property & property, const QString& value);
+
+    //! Toggles the mock's pen mode flag.
+    void togglePenMode();
+
+    //! Toggle the mock's touch flag.
+    void toggleTouch();
+
+Q_SIGNALS:
+
+    void profileChanged(const QString& profile);
+
+/*
+    void tabletAdded(const TabletInformation& info);
+
+    void tabletRemoved();
+*/
+public:
+    QString     m_lastDeviceGet;   //!< The last device a property was requsted from.
+    QString     m_lastDeviceSet;   //!< The last device a property was set on.
+    QString     m_lastPropertyGet; //!< The last property which was requested.
+    QString     m_lastPropertySet; //!< The last property which was set.
+
+    QString     m_property;        //!< The property value returned by this mock, no matter which property is requested.
+    QStringList m_profiles;        //!< The list of profiles returned by this mock.
+    QString     m_profile;         //!< The profile name returned by this mock.
+    bool        m_flagPenMode;     //!< The flag which resembles the pen mode state.
+    bool        m_flagTouch;       //!< The flag which resembles the touch state.
+
+
+}; // CLASS
+}  // NAMESPACE
+#endif // HEADER PROTECTION
