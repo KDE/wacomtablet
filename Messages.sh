0b007342569445843e2a67e4d377f1b232022b67
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2014-02-08 19:49:40 +0100

    Extract 2 *.pot catalogues (one for the app and one for the plasmoid)
    BUG: 330696
    FIXED-IN: 2.1

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/wacomtablet.pot
+$XGETTEXT `find . -path ./src/applet -prune -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/wacomtablet.pot
+$XGETTEXT `find ./src/applet -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/plasma_applet_tabletsettings.pot
 rm -f rc.cpp
