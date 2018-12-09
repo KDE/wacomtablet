35b3f18d6af221b195e7899f9a64444784ee7624
Valerii Malov <jazzvoid@gmail.com>
2018-12-09 19:59:56 +0300

    fix Messages.sh target for plasmoid

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -path ./src/applet -prune -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/wacomtablet.pot
-$XGETTEXT `find ./src/applet -name \*.qml -o -name \*.js` -o $podir/plasma_applet_org.kde.wacomtablet.pot
+$XGETTEXT `find ./src/applet -name \*.qml -o -name \*.js` -o $podir/plasma_applet_org.kde.plasma.wacomtablet.pot
 rm -f rc.cpp
