fdc9abdd352fb23eba3a381c3a595527ad6dfee1
Valerii Malov <jazzvoid@gmail.com>
2019-05-02 18:46:14 +0300

    Drop support for systems without xcb-xinput (part 1)
    Current debian & kubuntu should ship xcb-xinput since 1.13.1 or 1.13.2

--- src/common/x11inputdevice.cpp
+++
@@ -1,5 +0,0 @@
-#if defined(HAVE_XCB_XINPUT)
-#include "x11inputdevice-xcb.cpp"
-#else
-#include "x11inputdevice-xlib.cpp"
-#endif
--- src/kded/x11eventnotifier.cpp
+++
@@ -1,5 +0,0 @@
-#if defined(HAVE_XCB_XINPUT)
-#include "x11eventnotifier-xcb.cpp"
-#else
-#include "x11eventnotifier-xlib.cpp"
-#endif
